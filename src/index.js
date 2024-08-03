0afad6b4c99fc23366187493aee8c45149f567a4
user. name  taniasalmatwbm@gamil.com <taniasalmatwbm@gamil.com>
2024-08-04 00:19:10 +0500

    express server listner added

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import dotenv from 'dotenv' //improved
 dotenv.config({path:'./.env'})
 
 
-//import {app} from './app.js'
+import {app} from './app.js'
 
 import db_conect from "./db/index.js";
 
 
-
+//app db se connect hune k baad listen krti h
 db_conect()
+.then(()=>{
+    app.on('error', (error)=>{
+       console.log(`Error from app on listner ${error}`)
+       throw error
+    })
+
+   app.listen(process.env.PORT || 8000, ()=>{
+       console.log(`Serving is running on this port ${process.env.PORT || 8000}`)
+   })
+})
+.catch((err)=>{
+   console.log(`Running is coming from server listner ${err}`)
+})
