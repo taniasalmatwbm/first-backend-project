63fa7713fba02148d85108d3cb18f38be419ad09
user. name  taniasalmatwbm@gamil.com <taniasalmatwbm@gamil.com>
2024-08-05 22:32:50 +0500

    authmiddleware is made for logout

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,46 @@
+
+//login karne pr ham accessToken or reFreshToken bhje usi k base pr verify kare gy k 
+// user k pass sahi token h ya nhi usi pr login hua agr to true login h to ham 
+
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from 'jsonwebtoken'
+import { User } from "../models/user.model";
+//req.body mein ak new object yani ak or req set kr dein gy
+//jo cheez use na hu rhi hu _ lga lo
+export const varifyJwt = asyncHandler(async (req, _, next)=>{
+// token k acces lena h how to take
+// we can take access of req.cookie ye aye ko loginUser se app mein or phir app se kahi
+// b access hu sakta h 
+ // login se to sye hn cookie lkn phir b  
+ //postman se header mein Authorization: Bearer <token> ata h
+  try{
+    const token = req.cookies?.accessToken || req.header
+("Authorization")?.replace("Bearer ", "") //kali kr de tp pichy serf token reh jaye ga 
+
+if(!token){
+    throw new ApiError(201, "unAuthorized request")
+}
+
+//token agr h to jwt se varify kare gy yani decode 
+//token user.modle mein generateAccessToken method se jwt ko retuen kr rehy hn
+// esi liye jwt se varify kr sakte hn
+//token to a jayr ga lkn decode wahi kr sakta jis k pass secret key hu gi
+ const decodedToken =jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+ console.log(decodedToken)
+ //agr decodedToken user k id h to user k id ko req.user mein set kr dein gy
+ const user =await User.findById(decodedToken?._id) 
+ //_id user.modle se a rha h jo k jwt ko diya h
+ .select("-password -refreshToken") //ignore
+
+ if(!user){
+    // todo : discess about frontend
+    throw new ApiError(201, "inValid Token !!!")
+ }
+ req.user=user
+ //routes mein verifyJwt k bad kiya krna h es liye next() use kiya
+ next()
+  }catch(error){
+    throw new ApiError(401, error?.message || "Invalid token access")
+  }
+})
