eb976b71335c8760e2c80938905723c791a5f499
user. name  taniasalmatwbm@gamil.com <taniasalmatwbm@gamil.com>
2024-08-06 16:51:55 +0500

    subscriber model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type : Schema.Types.ObjectId, //one who is subscribing
+        ref: "User"
+    },
+    chennel: {
+        type: Schema.Types.ObjectId, //one who is being subscribed
+        ref: "User"
+    }
+
+})
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
